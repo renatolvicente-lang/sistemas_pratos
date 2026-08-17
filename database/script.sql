23a288ee1a4b39e8e7262e2318f44717af543ed0
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-08-17 10:10:07 -0300

    add - criação do script de criação do banco

--- database/script.sql
+++ database/script.sql
@@ -3,8 +3,18 @@ CREATE DATABASE sistemas_pratos;
 use sistemas_pratos;
 
 CREATE table usuarios(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR NOT NULL,
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR NOT NULL,
     senha INT NOT NULL
 
+);
+
+CREATE table pratos(
+    id_prato INT PRIMARY KEY AUTO_INCREMENT,
+    nome_prato VARCHAR NOT NULL,
+    descrição TEXT NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria VARCHAR NOT NULL,
+    id_usuario VARCHAR,
+    foreign KEY (id_usuario) references usuarios(id)
 );
